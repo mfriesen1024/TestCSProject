9cbbc94225407070e60d502f10e544b971c0dedf
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-01-05 11:35:48 -0400

    Add files.

---
+++ TestCSProj/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// A test project.
+
+namespace TestCSProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Welcome to the program. Press a key to continue.");
+            Console.ReadKey();
+            Console.WriteLine("Would you like to crash the program, or close it gracefully?");
+            Console.WriteLine("Type \"crash\" to crash, type \"close\" to close gracefully.");
+
+            string txt = Console.ReadLine();
+            switch (txt)
+            {
+                case "crash": throw new Exception("CRASH!!! >:)");
+                case "close": Environment.Exit(0); break;
+                default: throw new Exception("You didn't answer correctly, so CRASH!!! >:)");
+            }
+        }
+    }
+}
